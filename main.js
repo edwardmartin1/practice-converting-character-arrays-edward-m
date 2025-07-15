236946b740e783ddfbdf1fd3cd5732da2a463c26
edwardmartin1 <edwardmartin1@yahoo.com>
2025-07-15 12:48:51 -0500

    Completed code for Practice: Converting Character Arrays

---
+++ main.js
@@ -0,0 +1,29 @@
+let message = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let reversedMessage = message.split('').reverse().join('');
+console.log("Reversed message:", reversedMessage);
+
+message = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+reversedMessage = message.split('').reverse().join('');
+console.log("Reversed message:", reversedMessage);
+
+message = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI  .lanoisseforp a ekil leef ot evah t'nod uoY";
+reversedMessage = message.split('').reverse().join('');
+console.log("Reversed message:", reversedMessage);
+
+message = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+reversedMessage = message.split('').reverse().join('');
+console.log("Reversed message:", reversedMessage);
+
+
+let inspirationMessage = "I am worthy";
+let reversedInspirationMessage = inspirationMessage.split('').reverse().join('');
+console.log("Reversed inspiration message:", reversedInspirationMessage);
+
+inspirationMessage = "I am smart";
+reversedInspirationMessage = inspirationMessage.split('').reverse().join('');
+console.log("Reversed inspiration message:", reversedInspirationMessage);
+
+inspirationMessage = "I am strong";
+reversedInspirationMessage = inspirationMessage.split('').reverse().join('');
+console.log("Reversed inspiration message:", reversedInspirationMessage);
+
